31e62a6c09fc17cebadb959b7f43fd5e8452f2c2
knguyen-academy <knguyen.academy@gmail.com>
2018-05-06 22:24:28 -0700

    Added index page

---
+++ slideNav/slideNav.js
@@ -0,0 +1,11 @@
+function openNav()
+{
+    document.getElementById('slideNav1').style.width = '200px';
+    document.getElementById('main1').style.marginLeft = '200px';
+    
+}
+
+function closeNav() {
+    document.getElementById("slideNav1").style.width = "0";
+    document.getElementById("main1").style.marginLeft = "0";
+}
